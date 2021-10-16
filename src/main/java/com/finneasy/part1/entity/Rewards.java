1aff298fc9b8347e1addee5e4329fbd5129df220
malay <dynamic.malay@gmail.com>
2021-10-16 13:59:28 +0530

    Rewards entity added

--- src/main/java/com/finneasy/part1/entity/Rewards.java
+++ src/main/java/com/finneasy/part1/entity/Rewards.java
@@ -1,4 +1,31 @@
 package com.finneasy.part1.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
 public class Rewards {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String image_url;
+
+    private Date timestamp;
+
+    private Double price;
+
 }
